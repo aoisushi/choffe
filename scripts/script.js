5b4c375303734d67518e4f9859cb23bafd8dfa49
Sushi <kylieoiyingchan@gmail.com>
2023-05-26 18:00:45 -0700

    Revising responsive, adding slider

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,8 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
+  
+  
   // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
     zoom: 4,
